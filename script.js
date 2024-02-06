f362f77649c3db8d1f0fa4692e841a68d771425f
muyi2905 <obaremimuyiwa@gmail.com>
2024-02-06 11:46:16 +0000

    var declared

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const apiKey = '4a438fbea626ca3fb9578ce4d98bfe49';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
+const locationInput = document.getElementById('locationInput');
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
